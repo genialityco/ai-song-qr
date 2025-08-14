88b3a832081bfd3d2a69790709af7f1474394174
juanfe <lassojuanfe@gmail.com>
2025-08-14 16:27:29 -0500

    Eliminar NextDynamic

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -2,11 +2,7 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-import NextDynamic from "next/dynamic";
-
-const SurveyClient = NextDynamic(() => import("./SurveyClient"), {
-  ssr: false,
-});
+import SurveyClient from "./SurveyClient"; // import directo
 
 export default function Page() {
   return <SurveyClient />;
